7a3a3045df2af25f12b6498c32a79f1bc48f9046
Joel Berger <joel.a.berger@gmail.com>
2012-07-23 18:07:32 -0500

    added quine example

---
+++ examples/quine.pl
@@ -0,0 +1,26 @@
+use Mojolicious::Lite;
+use lib 'lib';
+
+plugin 'PPI';
+get '/' => sub {
+  my $self = shift;
+  $self->stash( file => __FILE__ );
+  $self->render('quine');
+};
+
+app->start;
+
+__DATA__
+
+@@ quine.html.ep
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>A Mojolicious Quine</title>
+    %= javascript 'ppi.js'
+    %= stylesheet 'ppi.css'
+  </head>
+  <body>
+    %= ppi $file;
+  </body>
+</html>
